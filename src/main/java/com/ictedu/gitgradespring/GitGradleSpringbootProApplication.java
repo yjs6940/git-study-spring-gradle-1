5b61a2b0056677a4c45fa5db54e9ffe6c3c7fcd7
ChoiCheolHwan <hwanyhee@naver.com>
2024-07-16 15:35:02 +0900

    Git 연습용 프로젝트 생성

---
+++ src/main/java/com/ictedu/gitgradespring/GitGradleSpringbootProApplication.java
@@ -0,0 +1,13 @@
+package com.ictedu.gitgradespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGradleSpringbootProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitGradleSpringbootProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ictedu/gitgradespring/GitGradleSpringbootProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictedu.gitgradespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGradleSpringbootProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
